e14a862540f1ecc969bb0d87fea2cb8735b6f623
Gabriel R <gabriel1706.com@gmail.com>
2023-01-19 21:54:05 -0300

    classes

--- Classes/PlayerClasses.cs
+++ Classes/PlayerClasses.cs
@@ -6,14 +6,99 @@ using System.Threading.Tasks;
 
 namespace RPGbot.db
 {
-	public class PlayerClasses
-	{
-		public string key { get; set; }
-		public string Fname { get; set; }
-		public string Mname { get; set; }
-		public string description { get; set; }
-		public int dice { get; set; }
+    public static class PlayerClasses
+    {
+        public static BarbaroClass Barbaro { get; }
+        public static BardoClass Bardo { get; }
+        public static BruxoClass Bruxo { get; }
+        public static ClerigoClass Clerigo { get; }
+        public static DruidaClass Druida { get; }
+        public static FeiticeiroClass Feiticeiro { get; }
+        public static GuerreiroClass Guerreiro { get; }
+        public static LadinoClass Ladino { get; }
+        public static MagoClass Mago { get; }
+        public static MongeClass Monge { get; }
+        public static PaladinoClass Paladino { get; }
+        public static PatrulheiroClass Patrulheiro { get; }
+    }
 
-		//public enum Pericias { adestrar }
-	}
+    public interface PlayerClass
+    {
+        string Fname { get; }
+        string Mname { get; }
+        int dice { get; }
+    }
+
+    public class BarbaroClass : PlayerClass
+    {
+        public string Mname => "Bárbaro";
+        public string Fname => "Bárbara";
+        public int dice => 12;
+    }
+    public class BardoClass : PlayerClass
+    {
+        public string Mname => "Bardo";
+        public string Fname => "Barda";
+        public int dice => 8;
+    }
+    public class BruxoClass : PlayerClass
+    {
+        public string Mname => "Bruxo";
+        public string Fname => "Bruxa";
+        public int dice => 8;
+    }
+    public class ClerigoClass : PlayerClass
+    {
+        public string Mname => "Clérigo";
+        public string Fname => "Clériga";
+        public int dice => 8;
+    }
+    public class DruidaClass : PlayerClass
+    {
+        public string Mname => "Druida";
+        public string Fname => "Druidesa";
+        public int dice => 8;
+    }
+    public class FeiticeiroClass : PlayerClass
+    {
+        public string Mname => "Feiticeiro";
+        public string Fname => "Feiticeira";
+        public int dice => 6;
+    }
+    public class GuerreiroClass : PlayerClass
+    {
+        public string Mname => "Guerreiro";
+        public string Fname => "Guerreira";
+        public int dice => 10;
+    }
+    public class LadinoClass : PlayerClass
+    {
+        public string Mname => "Ladino";
+        public string Fname => "Ladina";
+        public int dice => 8;
+    }
+    public class MagoClass : PlayerClass
+    {
+        public string Mname => "Mago";
+        public string Fname => "Maga";
+        public int dice => 6;
+    }
+    public class MongeClass : PlayerClass
+    {
+        public string Mname => "Monge";
+        public string Fname => "Monja";
+        public int dice => 8;
+    }
+    public class PaladinoClass : PlayerClass
+    {
+        public string Mname => "Paladino";
+        public string Fname => "Paladina";
+        public int dice => 10;
+    }
+    public class PatrulheiroClass : PlayerClass
+    {
+        public string Mname => "Patrulheiro";
+        public string Fname => "Patrulheira";
+        public int dice => 10;
+    }
 }
--- Modules/AddPlayerModule.cs
+++ Modules/AddPlayerModule.cs
@@ -10,49 +10,65 @@ using System.Threading.Tasks;
 
 namespace RPGbot.Modules
 {
-	public class AddPlayerModule : InteractionModuleBase<SocketInteractionContext>
-	{
-		public InteractionService Commands { get; set; }
+    public class AddPlayerModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        public InteractionService Commands { get; set; }
 
-		public AddPlayerModule(DiscordSocketClient client) => _client = client;
-		private readonly DiscordSocketClient _client;
+        public AddPlayerModule(DiscordSocketClient client) => _client = client;
+        private readonly DiscordSocketClient _client;
 
 
-		[SlashCommand("add", "Adiciona um personagem ao jogo. Só é permitido 1 personagem por jogador")]
-		public async Task HandleAddCommand()
-		{
-			Player player = DbHandler.LoadPlayer(Context.User.Id.ToString());
-			if (player != null)
-			{
-				await RespondAsync($"Você já tem um personagem! {player.name} já existe.", null, false, true); return;
-			}
+        [SlashCommand("add", "Adiciona um personagem ao jogo. Só é permitido 1 personagem por jogador")]
+        public async Task HandleAddCommand()
+        {
+            try
+            {
+                Player player = DbHandler.LoadPlayer(Context.User.Id.ToString());
+                if (player != null)
+                {
+                    await RespondAsync($"Você já tem um personagem! {player.name} já existe.", null, false, true); return;
+                }
 
-			await RespondAsync();
-		}
-	}
+                var classeMenu = new SelectMenuBuilder()
+                {
+                    CustomId = "classeMenu",
+                    Placeholder = "Escolha sua classe"
+                };
+
+                var component = new ComponentBuilder()
+                    .WithSelectMenu(classeMenu);
+
+                await RespondAsync($"Escolha a classe e a raça do personagem", components: component.Build());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
 }
 
 public class AddModal : IModal
 {
-	public string Title => "Crie seu personagem!";
+    public string Title => "Crie seu personagem!";
 
-	[InputLabel("Nome completo do jogador")]
-	[ModalTextInput("jogador", TextInputStyle.Short, "Seu nome...", maxLength:100)]
-	public string jogador { get; set; }
+    [InputLabel("Nome completo do jogador")]
+    [ModalTextInput("jogador", TextInputStyle.Short, "Seu nome...", maxLength: 100)]
+    public string jogador { get; set; }
 
-	[InputLabel("Nome do personagem")]
-	[ModalTextInput("name", TextInputStyle.Short, "Nome do personagem...", maxLength:40)]
-	public string name { get; set; }
+    [InputLabel("Nome do personagem")]
+    [ModalTextInput("name", TextInputStyle.Short, "Nome do personagem...", maxLength: 40)]
+    public string name { get; set; }
 
-	[InputLabel("Idade do personagem (número inteiro)")]
-	[ModalTextInput("age", TextInputStyle.Short, "Idade do personagem...", maxLength: 3)]
-	public string age { get; set; }
+    [InputLabel("Idade do personagem (número inteiro)")]
+    [ModalTextInput("age", TextInputStyle.Short, "Idade do personagem...", maxLength: 3)]
+    public string age { get; set; }
 
-	[InputLabel("Peso do personagem (número inteiro em kg)")]
-	[ModalTextInput("weight", TextInputStyle.Short, "Peso do personagem...", maxLength: 3)]
-	public string weight { get; set; }
+    [InputLabel("Peso do personagem (número inteiro em kg)")]
+    [ModalTextInput("weight", TextInputStyle.Short, "Peso do personagem...", maxLength: 3)]
+    public string weight { get; set; }
 
-	[InputLabel("Altura do personagem (em cm)")]
-	[ModalTextInput("height", TextInputStyle.Short, "Altura do personagem...", maxLength: 3)]
-	public string height { get; set; }
+    [InputLabel("Altura do personagem (em cm)")]
+    [ModalTextInput("height", TextInputStyle.Short, "Altura do personagem...", maxLength: 3)]
+    public string height { get; set; }
 }
